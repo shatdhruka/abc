b603cd21e7e050908a9a586dffdedc23f3d07965
shatdhruka <42232515+shatdhruka@users.noreply.github.com>
2019-02-09 12:12:43 +0530

    Create between two number.c

---
+++ between two number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void)
+{
+    int n,m,x;
+    scanf("%d %d",&n,&m);
+    x=n+m;
+    if(x%2==0)
+    {
+        printf("even",x);
+    }else
+    {
+        printf("odd",x);
+    }
+}
