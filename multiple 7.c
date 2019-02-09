ed2e9d70fcd122ca9016140f45ef1d47232e6f84
shatdhruka <42232515+shatdhruka@users.noreply.github.com>
2019-02-09 20:10:43 +0530

    Create multiple 7.c

---
+++ multiple 7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void)
+{
+    int n,x;
+    scanf("%d",&n);
+    if((n%7)==0)
+    {
+        printf("yes",n);
+    }
+    else
+    {
+        printf("no",n);
+    }
+}
